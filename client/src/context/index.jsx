438d1afcdb6850dcc9a8a5a6df0cc226f992c0d7
Reginald Osano <regie30developer@gmail.com>
2022-11-28 06:34:28 +0800

    Initial commit and pushed

---
+++ client/src/context/index.jsx
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useEffect, useRef, useState } from 'react';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+import { useNavigate } from 'react-router-dom';
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => {
+	const [walletAddress, setWalletAddress] = useState("");
+
+	const updateCurrentWalletAddress = async () => {
+		const accounts = await window.ethereum.request({
+			method: 'eth_requestAccounts'
+		});
+		console.log(accounts);
+	}
+	
+	useEffect(() => {
+		updateCurrentWalletAddress();
+	}, []);
+
+	useEffect(() => {
+		const setSmartcontractAndProvider = async () => {
+			const web3Modal = new Web3Modal();
+			const connection = await web3Modal.connect();
+			const newProvider = new ethers.providers.Web3Provider(connection);
+			const signer = newProvider.signer();
+			const newContract = new ethers.Contract();
+		}
+	})
+
+
+
+	return (
+		<GlobalContext.Provider value={{
+			demo: 'test' 
+		}}>
+			{children}
+		</GlobalContext.Provider>
+	)
+}
+
+export const useGlobalContext = () => useContext(GlobalContext);
---
+++ client/src/page/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { PageHOC } from '../components';
+import { useGlobalContext } from '../context';
+
+const Home = () => {
+
+  const { demo } = useGlobalContext();
+
+  return (
+    <div>
+      <h1 className="text-white text-xl">{demo}</h1>
+    </div>
+  )
+};
+
+export default PageHOC(
+  Home,
+  <>Welcome to Regie Gods <br /> a Web3 NFT Card Game</>,
+  <>Connect your wallet to start playing <br /> the ultimate 
+  Web3 Battle Card Game</>
+ );
